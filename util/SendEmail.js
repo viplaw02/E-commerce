f4cb80615d3574acb06806f2b6063dae35e9123e
viplaw02 <viplawsinha1998@gmail.com>
2024-12-31 18:11:16 +0530

    commit

--- util/SendEmail.js
+++ util/SendEmail.js
@@ -1,6 +1,8 @@
 const  nodemailer = require('nodemailer')
 const {otp} = require('../model/otp');
-exports.SendMail = async (email,title,otp)=>{
+ const {HttpStatusCode}= require('axios')
+ require('dotenv').config();
+exports.SendMail = async (email,title,body)=>{
  try {
      const Transporter = nodemailer.createTransport({
         host:process.env.MAIL_HOST,
@@ -11,10 +13,19 @@ exports.SendMail = async (email,title,otp)=>{
         pass:process.env.MAIL_PASS,
         })
      })
-
+     const Sendmail = await Transporter.sendMail({
+        from:process.env.MAIL_USER,
+        to:email,
+        subject:title,
+        html:`${body}`
+     })
+    console.log("send mail",Sendmail);
+    return Sendmail;
  } catch (error) {
-    
+    console.log(error.message);
+    return res.status(HttpStatusCode.InternalServerError).json({
+        success:false,
+       message:`Internal Server error ${error.message}`
+    })
  }
-
-
 } 
